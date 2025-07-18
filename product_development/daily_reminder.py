194e332ba157a720688749e7564c5dc7059ce3a0
Angelica Adjei-Kwarteng <angelicaakwarteng24@gmail.com>
2025-07-18 16:41:51 +0000

    Python Projects
    Split projects into data science and product development (software)

---
+++ product_development/daily_reminder.py
@@ -0,0 +1,21 @@
+# Personal Daily Reminder
+task = input("Enter your task: ")
+priority = str(input("Priority (high/medium/low): "))
+time_bound = str(input("Is it time-bound? (yes/no): "))
+
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"Task: '{task}' is a {priority} priority task that requires immediate attention today!")
+        else:
+            print(f"Reminder: This is a {priority} priority task.")
+    case "medium":
+        if time_bound == "yes":
+            print(f"Reminder: 'Don't forget the deadline'. This is a {priority} priority task that requires attention!")
+        else:
+            print(f"Reminder: This is a {priority} priority task.")
+    case "low":
+        if time_bound == "yes":
+            print(f"Reminder: This is a {priority} priority task but it is time-bound.")
+        else:
+            print(f"'Read a book' is a low priority task. Consider completing it when you have free time.")
---
+++ product_development/match_case_calculator.py
@@ -0,0 +1,20 @@
+#performing calculation using case
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+
+match operation:
+    case "+":
+        print(f"The result is: {num1 + num2}.")
+    case "-":
+        print(f"The result is: {num1 - num2}.")
+    case "*":
+        print(f"The result is: {num1*num2}.")
+    case "/":
+        if num2 == 0:
+            print("cannot divide by zero")
+        else:
+            print(f"The result is: {num1/num2}.")
+    case _:
+        print("Invalid operation.")
